12c6c9621b78b0e21f5cdd2f6d0faaf320b3a5be
manzoku-bukuro <manzoku.bukuro@gmail.com>
2022-10-25 23:59:54 +0900

    TypeScriptサンプル

--- src/components/TypeScriptExample.vue
+++ src/components/TypeScriptExample.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 // import TypeScriptExample from '@/components/TypeScriptExample.vue';
 import { ref } from 'vue';
+
 // テキスト
 const stringSample = ref<string>("string");
 // 数字
@@ -11,9 +12,21 @@ const arrayExample = ref<number[]>([1, 2, 3]);
 const mixArrayExample = ref<(number | string)[]>([1, 'string']);
 // 配列(generics)
 const genericsArrayExample = ref<Array<string | number>>([2, 'apple']);
-const randomKeyDictionaryExample = ref<{ [key: string]: number }>({ 'a': 1, 'b': 2 });
-// const randomKeyDictionaryExample = ref<({ [key: string]: number } | string)[]>([{ 'a': 1 }, { 'b': 2 }, 'c']);
-const sample = ref<Array<number>>([1, 2, 3, 4])
+// 連想配列 キー名が決まっている場合
+const fixedKeyDictionaryExample = ref<{ name: string, count: number, unnecessary?: string }>({ 'name': 'a', 'count': 1 });
+// 連想配列 キー名が決まっていない場合
+const randomKeyDictionaryExample = ref<{ [key: string]: string | number }>({ 'a': 1, 'b': 'cc' });
+
+// interface
+interface myInterface {
+    name: string | null;
+    count: number;
+    isSet?: boolean;
+    info: {
+        status: string;
+    } | null;
+}
+const interfaceExample = ref<myInterface[]>([{ name: 'test1', count: 1, info: { status: 'ok' } },{ name: 'test2', count: 2, isSet: true, info: null }]);
 </script>
 
 <template>
@@ -22,7 +35,9 @@ const sample = ref<Array<number>>([1, 2, 3, 4])
     <div>{{ arrayExample }}</div>
     <div>{{ mixArrayExample }}</div>
     <div>{{ genericsArrayExample }}</div>
+    <div>{{ fixedKeyDictionaryExample }}</div>
     <div>{{ randomKeyDictionaryExample }}</div>
+    <div>{{ interfaceExample }}</div>
 </template>
 
 <style lang="scss" scoped>
