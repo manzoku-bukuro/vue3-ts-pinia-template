03c46bfb414fdafddbb7a798b0598fc536f6f2a2
manzoku-bukuro <manzoku.bukuro@gmail.com>
2023-02-04 17:28:34 +0900

    refsの使い方

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue';
 import TypeScriptExample from '@/components/TypeScriptExample.vue';
+import RefSample from '@/components/RefSample.vue';
+
+const refSample = ref<InstanceType<typeof RefSample> | null>(null);
+onMounted(() => {
+  console.log(refSample.value?.count);
+  refSample.value?.countUp();
+  console.log(refSample.value?.callDoubleCount());
+})
 </script>
 
 <template>
   <div>
     <TypeScriptExample></TypeScriptExample>
+    <RefSample ref="refSample"></RefSample>
   </div>
 </template>
 
---
+++ src/components/RefSample.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const count = ref<number>(0);
+
+const countUp = () => {
+    count.value++;
+}
+
+const callDoubleCount = ():number => {
+    return count.value * 2;
+}
+
+defineExpose({
+    count,
+    countUp,
+    callDoubleCount,
+});
+</script>
+
+<template>
+    <div></div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
